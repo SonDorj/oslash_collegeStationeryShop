8207be0dc7233e6cce222e1e4a0fa89c04680a24
Sonam <dsonam956@gmail.com>
2022-11-24 22:43:00 +0530

    almost finalizing

--- run.sh
+++ run.sh
@@ -4,3 +4,5 @@ python3 -m geektrust sample_input/input1.txt
 python3 -m geektrust sample_input/input2.txt
 python3 -m geektrust sample_input/input3.txt
 python3 -m geektrust sample_input/input4.txt
+python3 -m geektrust sample_input/Output1.txt
+python3 -m geektrust sample_input/Output2.txt
